38961319adb8a3b902c6e6783583a89f047d759c
Jimmy <jimagile@gmail.com>
2015-06-22 17:22:46 +0800

    finish hw2: add HW2 #3

--- labs-answ/code/hw2/Arrays.java
+++ labs-answ/code/hw2/Arrays.java
@@ -4,7 +4,8 @@
 /** HW #2, Problem #2. */
 
 /** Array utilities.
- *  @author
+ *  @author jimmy
+ *  @date 2015-06-22
  */
 class Arrays {
     /* 2a. */
@@ -12,17 +13,55 @@ class Arrays {
      *  the elements of B. */
     static int[] catenate(int[] A, int[] B) {
         /* *Replace this body with the solution. */
-        return null;
+        int[] ret = new int[A.length+B.length];
+        
+        if (A == null || B == null) {
+            int[] src = (A == null ? B:A);
+            intcopy (ret, 0, src, 0, src.length);
+            return ret;
+        }
+        intcopy (ret, 0, A, 0, A.length);
+        intcopy (ret, A.length, B, 0, B.length);
+        return ret;
     }
-
+    /** copy {src[sfrom]... src[sto-1]}, to dst[dfrom] ...
+     *   */
+    private static int[] intcopy(int[] dst, int dfrom, int[] src, int sfrom, int sto) {
+        if (dst == null) {
+            throw new IllegalArgumentException("dst null");
+        }
+        if (src == null) {
+            return dst;
+        }
+        if (sto > src.length || sfrom > sto) {
+            throw new IllegalArgumentException("sfrom:" + sfrom + "sto:" + sto + "len:" + src.length);
+        }
+        for (int i = 0; i < sto - sfrom; i +=1) {
+            dst[dfrom + i] = src[sfrom + i];
+        }
+        return dst;
+    }
+    
     /* 2b. */
     /** Returns the array formed by removing LEN items from A,
      *  beginning with item #START. */
     static int[] remove(int[] A, int start, int len) {
         /* *Replace this body with the solution. */
-        return null;
+        /* should not out of bound */
+        if (start < 0 || start > A.length - 1) {
+            throw new IllegalArgumentException(start +" "+ len);
+        }
+        if (start + len > A.length) {
+            throw new IllegalArgumentException(start +" " + len);
+        }
+        int[] ret = new int[A.length - len];
+        intcopy(ret, 0, A, 0, start);
+        if (start + len <= A.length) {
+            intcopy(ret, start, A, start + len, A.length);
+        }
+        return ret;
     }
-
+    
     /* 2c. */
     /** Returns the array of arrays formed by breaking up A into
      *  maximal ascending lists, without reordering.
@@ -31,6 +70,36 @@ class Arrays {
      *  {{1, 3, 7}, {5}, {4, 6, 9, 10}}. */
     static int[][] naturalRuns(int[] A) {
         /* *Replace this body with the solution. */
-        return null;
+        int[][] tmpret = new int[A.length][];  /* don't know diamension, how to deal, cast ? or have another method */
+        
+        if (A.length == 0) {
+            return new int[0][0];
+        }
+        
+        if (A.length == 1) {
+            return new int[][] {{A[0]},};
+        }
+        /* A.length > 1 */
+        int n = 0;
+        int ascstart = 0;
+        int ascend = 1;
+        for (; ascend <= A.length - 1; ascend += 1) {
+            if (A[ascend] < A[ascend-1]) {
+                /* the sep point */
+                int[] newasc = new int[ascend - ascstart];
+                intcopy(newasc, 0, A, ascstart, ascend);
+                tmpret[n] = newasc;
+                n += 1;
+                ascstart = ascend;
+            }
+        }
+        tmpret[n] = intcopy(new int[ascend - ascstart], 0, A, ascstart, ascend);
+        n += 1;
+        
+        int[][] ret = new int[n][];
+        for (int i = 0; i < n; i++) {
+            ret[i] = tmpret[i];
+        }
+        return ret;
     }
 }
--- labs-answ/code/hw2/HW2Test.java
+++ labs-answ/code/hw2/HW2Test.java
@@ -1,11 +1,105 @@
 /** java HW2Test should test the methods defined in Lists and Arrays.
- *  @author
+ *  @author jimmy
+ *  @date   2015-06-22
  */
+ 
+ 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 public class HW2Test {
 
     /** Run all tests. */
     public static void main(String[] args) {
         // FILL THIS IN
+        System.exit(jh61b.junit.textui.runClasses(HW2Test.class));
+        
     }
-
+    
+    @Test public void naturalRuns() {
+        int [][][] testmatrix = 
+        {
+            {{1}},
+            {{1, 3}},
+            {{1, 3, 7}},
+            {{1, 3, 7}, {5}, {4, 6}},
+            {{1, 3, 7}, {5}, {4, 6, 9, 10}},
+            {{1, 3, 7}, {5}, {4, 6, 9, 10}, {-1}, {-3, -1, 64564}},
+        };
+        
+        IntList[] L1 = new IntList[6];
+        L1[0] = IntList.list(1);
+        L1[1] = IntList.list(1, 3);
+        L1[2] = IntList.list(1, 3, 7);
+        L1[3] = IntList.list(1, 3, 7, 5, 4, 6);
+        L1[4] = IntList.list(1, 3, 7, 5, 4, 6, 9, 10);
+        L1[5] = IntList.list(1, 3, 7, 5, 4, 6, 9, 10, -1, -3, -1, 64564);
+        
+        for (int i = 0; i < testmatrix.length; i++) {
+            IntList2 L1ret = Lists.naturalRuns(L1[i]);
+            //System.out.print(L1ret.toString() + "\n");
+            assertTrue("should be equal", Utils.equals(L1ret, testmatrix[i]));
+        }
+    }
+    
+    @Test public void catenate() {
+        int [][] testcase = 
+        {
+            {-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        };
+        
+        int [] expect = testcase[0];
+        for (int i = 0; i <= testcase[0].length; i ++) {
+            int[] a = Utils.subarray(testcase[0], 0, i);
+            int[] b = Utils.subarray(testcase[0], i, testcase[0].length - i);
+            System.out.print("catenate a:" + Utils.toString(a) + Utils.toString(b));
+            int[] actual = Arrays.catenate(a, b);
+            System.out.print("\t-->" + Utils.toString(actual) + "\n");
+            assertArrayEquals("should be equal", expect, actual);
+        }
+    }
+    
+     @Test public void remove() {
+        int [][] testcase = 
+        {
+            {-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+        };
+        
+        for (int i = 0; i <= testcase[0].length; i ++) {
+            int[] a = Utils.subarray(testcase[0], 0, i);
+            int[] b = Utils.subarray(testcase[0], i, testcase[0].length - i);
+            System.out.print("remove: a:" + Utils.toString(a) + Utils.toString(b));
+            int [] expect = b;
+            int[] actual = Arrays.remove(testcase[0], 0, i);
+            System.out.print("\t-->" + Utils.toString(actual) + "\n");
+            assertArrayEquals("should be equal", expect, actual);
+        }
+    }
+    
+    @Test public void naturalRuns_array() {
+        int [][][] testmatrix = 
+        {
+            {{1}},
+            {{1, 3}},
+            {{1, 3, 7}},
+            {{1, 3, 7}, {5}, {4, 6}},
+            {{1, 3, 7}, {5}, {4, 6, 9, 10}},
+            {{1, 3, 7}, {5}, {4, 6, 9, 10}, {-1}, {-3, -1, 64564}},
+        };
+        
+        int[][] A1 = new int[6][];
+        A1[0] = new int[]{1};
+        A1[1] = new int[]{1, 3};
+        A1[2] = new int[]{1, 3, 7};
+        A1[3] = new int[]{1, 3, 7, 5, 4, 6};
+        A1[4] = new int[]{1, 3, 7, 5, 4, 6, 9, 10};
+        A1[5] = new int[]{1, 3, 7, 5, 4, 6, 9, 10, -1, -3, -1, 64564};
+        
+        for (int i = 0; i < testmatrix.length; i++) {
+            int[][] Aret = Arrays.naturalRuns(A1[i]);
+            System.out.print("naturalRuns_array" + Utils.toString(testmatrix[i]) + "\t --> " + Utils.toString(Aret) + "\n");
+            assertTrue("should be equal", Utils.equals(Aret, testmatrix[i]));
+        }
+    }
 }
