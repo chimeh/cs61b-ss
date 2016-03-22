9aeb425d00a2e6ca22a83332f0b1ee9a083dc257
Jimmy <jimagile@gmail.com>
2016-03-22 23:59:42 +0800

    lab6 coding

--- labs-answ/code/lab6/TrivialFilter.java
+++ labs-answ/code/lab6/TrivialFilter.java
@@ -3,18 +3,20 @@ import utils.Filter;
 
 /** A kind of Filter that lets all the VALUE elements of its input sequence
  *  through.
- *  @author You
+ *  @author jimmy
+ *  @date 2016-03-22
  */
 class TrivialFilter<Value> extends Filter<Value> {
 
     /** A filter of values from INPUT that simply delivers all of them. */
     TrivialFilter(Iterator<Value> input) {
         super(input);
-        // FIXME
     }
+
+
 
     @Override
     protected boolean keep() {
-        return false;    // FIXME
+        return true;
     }
 }
---
+++ labs-answ/code/lab6/TrivialFilterTest.java
@@ -0,0 +1,29 @@
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jimmy on 2016-3-22.
+ */
+public class TrivialFilterTest {
+
+    @org.junit.Test
+    public void testIterator() throws Exception {
+        ArrayList<Integer> tv = new ArrayList();
+        for (int i; i < 30; i++) {
+            tv.add(i);
+        }
+        TrivialFilter<Integer> tfiter = new TrivialFilter(tv.iterator());
+        int j =0;
+        for(Iterator<Integer> i = tfiter.iterator(); i.hasNext();) {
+            Integer a = i.next();
+            j++;
+            Integer b = tv.get(j);
+            assertEquals(a, b);
+            System.out.print(a + "=?" + b);
+        }
+    }
+}
